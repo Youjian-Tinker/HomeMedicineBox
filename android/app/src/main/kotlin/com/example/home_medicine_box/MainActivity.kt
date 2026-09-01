428a4a49031e15f5b8c8d762bc161822118212d1
Youjian-Tinker <youjiannairuo@163.com>
2026-09-01 10:50:48 +0800

    chore: clear repository contents

--- android/app/src/main/kotlin/com/example/home_medicine_box/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.home_medicine_box
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'src/app/home_medicine_box_app.dart';
-
-void main() {
-  runApp(const ProviderScope(child: HomeMedicineBoxApp()));
-}
